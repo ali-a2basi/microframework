083e0de2611695dbe7afd1fcdfdd7f9160caabf2
Ali Abbasi <ali.abasi@protonmail.com>
2023-11-20 16:17:37 +0330

    Before Dispather

--- App/Core/Routing/Router.php
+++ App/Core/Routing/Router.php
@@ -39,41 +39,47 @@ class Router{
 
     private function dispatch404(){
         header("HTTP/1.1 404 Not Found");
-        include BASEPATH.'/views/errors/404.php';
+        view('errors.404');
         die();
     }
 
     private function dispatch405(){
         header("HTTP/1.1 405 Method Not Allowed");
-        include BASEPATH.'/views/errors/405.php';
+        view('errors.405');
         die();
     }
 
 
     private function methodIsInvalid(Request $request){
 
-        return true;
-        // return in_array($request->get_method(), $this->routes['methods'])? false : true ;
+        foreach($this->routes as $route){
+            return in_array($request->get_method(), $route['methods']) ? false : true ;
+         }
+        // 
 
 
     }
     public function run(){
 
-        #404 not found uri
-
-        if(is_null($this->current_route)){
-
-            
-            $this->dispatch404();
-        }
-
-
-
+        
         #405 method not supported
 
-        // if($this->methodIsInvalid($this->request)){
-        //     echo 'method is invalid';
-        //     $this->dispatch405();
-        // }
+        if($this->methodIsInvalid($this->request)){
+            $this->dispatch405();             
+         }
+
+        #404 not found uri
+
+         if(is_null($this->current_route)){
+            $this->dispatch404();
+            }
+
+
+        $this->dispatcher();
     }
+
+    private function dispatcher(){
+        
+    }
+
 }
--- helpers/helpers.php
+++ helpers/helpers.php
@@ -23,6 +23,21 @@ function randoElement($arr){
 }
 
 
+function view($path){
+    #input errors.404
+    #/views/errors/404.php
+
+    //changing errors.php to errors/php
+    $changePath = str_replace('.', '/' , $path );
+
+    //creating full Path
+    $fullPath = BASEPATH."views/$changePath.php";
+
+    include_once $fullPath;
+
+}
+
+
 
 
 
